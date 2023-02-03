5bd6106ac4816f75cd2271227a260247502c6063
shyarnis <shyarnisgg88@gmail.com>
2023-02-03 11:57:59 +0545

    feat(users): register a user

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -16,12 +16,12 @@ router.post("/register", async (req, res) => {
     try {
         // hash a req.body.password
         const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        const hashPassword = await bcrypt.hash(password, salt);
 
         // create a new user
         const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
+            username: username,
+            email: email,
             password: hashPassword,
         });
         const user = await newUser.save();
@@ -31,16 +31,24 @@ router.post("/register", async (req, res) => {
     }
 });
 
-// get a user
-// router.get("/register", async (req, res) => {
-//     const user = await new User({
-//         username: "John",
-//         email: "John@gmail.com",
-//         password: "123456",
-//     });
+// login a user
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
 
-//     await user.save();
-//     res.send("user is created");
-// });
+    try {
+        // find user in db by email
+        const user = await User.findOne({ email });
+        !user && res.status(404).json("user not found");
+
+        // password validator, compare
+        const validPassword = await bcrypt.compare(password, user.password);
+        !validPassword && res.status(400).json("wrong password");
+
+        // login user
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
