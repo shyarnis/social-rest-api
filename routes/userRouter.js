3768ba8ae24980df6d39e7bab9bfb9eef8596897
shyarnis <shyarnisgg88@gmail.com>
2023-02-03 21:06:58 +0545

    feat(users): update a user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,38 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
+const User = require("../models/userModel");
 
-// Get a user
-router.get("/", (req, res) => {
-    res.send("User is get");
+// UPDATE user with /:id
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.user.isAdmin) {
+        // check for password
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (error) {
+                return res.status(500).json(error);
+            }
+        }
+
+        // time to update a user
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Account has been updated");
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    } else {
+        return res.status(403).json("you can only update your account");
+    }
 });
+
+// delete user
+// get a user
+// follow a user
+// unfollow a user
 
 module.exports = router;
