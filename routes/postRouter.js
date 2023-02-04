69a6803e986c996335f0e19d41275e741b61f40f
shyarnis <shyarnisgg88@gmail.com>
2023-02-04 14:12:06 +0545

    feat(posts): delete a post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -32,6 +32,22 @@ router.put("/:id", async (req, res) => {
 });
 
 // delete a post
+router.delete("/:id", async (req, res) => {
+    try {
+        // find post by id
+        const post = await Post.findById(req.params.id);
+        if ((post.userId = req.body.userId)) {
+            // delete post
+            await post.deleteOne();
+            res.status(200).json("Post have been deleted");
+        } else {
+            res.status(403).json("You can delete only your post");
+        }
+    } catch (error) {
+        res.status(500).json({ error: "User id not found" });
+    }
+});
+
 // get a post
 // get timeline post
 // like and dislike post
