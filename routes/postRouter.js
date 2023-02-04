c278c7a938df0c4c308422a4ee14a3c6e66af99f
shyarnis <shyarnisgg88@gmail.com>
2023-02-04 15:53:29 +0545

    feat(posts): get all timeline posts

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -63,10 +63,35 @@ router.put("/:id/like", async (req, res) => {
             await post.updateOne({ $pull: { likes: req.body.userId } });
             res.status(200).json("Post has been disliked");
         }
-    } catch (error) {}
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
-// get timeline post
-// like and dislike post
+// get a post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// get timeline posts
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+        res.json(userPosts.concat(...friendPosts));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
