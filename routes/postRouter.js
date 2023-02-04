19f7f6af8e701ace4d6fdc6c15b540e482eb76ae
shyarnis <shyarnisgg88@gmail.com>
2023-02-04 13:56:10 +0545

    feat(posts): create a post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/postModel");
 
-// test
-router.get("/", (_, res) => {
-    res.send("Hello from Post");
-    console.log("OP");
+// create a post
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
+
+// update a post
+// delete a post
+// get a post
+// get timeline post
+// like and dislike post
 
 module.exports = router;
