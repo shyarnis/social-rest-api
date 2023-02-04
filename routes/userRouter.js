e0b61fba1a4ef2a99e254813b346cd93f0e859c0
shyarnis <shyarnisgg88@gmail.com>
2023-02-04 12:57:09 +0545

    feat(users): follow another user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -60,6 +60,32 @@ router.get("/:id", async (req, res) => {
 });
 
 // follow a user
+router.put("/:id/follow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            // user -> to be followed
+            // currentUser -> oneself
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+
+            // does "user" has followers includes "currentUser"
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $push: { followers: req.body.userId } });
+                await currentUser.updateOne({
+                    $push: { followings: req.params.id },
+                });
+                res.status(200).json("User has been followed");
+            } else {
+                res.status(403).json("You'd already followed this user");
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("You can't follow yourself");
+    }
+});
+
 // unfollow a user
 
 module.exports = router;
